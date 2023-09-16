4c99ea5ff0dd8591eaacba8c08c6db781333c70c
pi-tau <pi.tau@yahoo.com>
2023-09-16 22:19:25 +0200

    Update environment loop to work with all agents

--- src/environment_loop.py
+++ src/environment_loop.py
@@ -4,6 +4,7 @@ import pickle
 import time
 import warnings
 
+import matplotlib.pyplot as plt
 import numpy as np
 import torch
 from tqdm import tqdm
@@ -15,7 +16,8 @@ def environment_loop(seed, agent, env, num_iters, steps, log_dir, log_every=1, d
     Args:
         seed: int
             Seed for random number generation.
-        agent: RL agent object
+        agent: Agent
+            RL agent object implementing `policy` and `update` methods.
         env: gym.VectorEnv
             Vectorized environment conforming to the gym API.
         num_iters: int
@@ -45,7 +47,11 @@ def environment_loop(seed, agent, env, num_iters, steps, log_dir, log_every=1, d
     run_ret, run_len = np.nan, np.nan
     for i in tqdm(range(num_iters)):
         # Allocate tensors for the rollout observations.
-        obs = np.zeros(shape=(steps, num_envs, *env.single_observation_space.shape), dtype=np.float32)
+        obs = np.zeros(
+            shape=(steps, num_envs, *env.single_observation_space.shape),
+            dtype=np.float32,
+        )
+        next_obs = np.zeros_like(obs)
         actions = np.zeros(shape=(steps, num_envs), dtype=int)
         rewards = np.zeros(shape=(steps, num_envs), dtype=np.float32)
         done = np.zeros(shape=(steps, num_envs), dtype=bool)
@@ -62,7 +68,15 @@ def environment_loop(seed, agent, env, num_iters, steps, log_dir, log_every=1, d
 
             actions[s] = acts
             rewards[s] = r
+            next_obs[s] = o
             done[s] = (t | tr)
+
+            # If some environment was truncated, then extract the final obs from
+            # the returned info.
+            if tr.any():
+                for k in range(num_envs):
+                    next_obs[s][k] = o[k] if not tr[k] else infos["final_observation"][k]
+                # next_obs = np.where(tr, infos["final_observation"], o)
 
             # If any of the environments is done, then save the statistics.
             if done[s].any():
@@ -74,14 +88,21 @@ def environment_loop(seed, agent, env, num_iters, steps, log_dir, log_every=1, d
                 ])
                 terminated += sum((1 for i in range(num_envs) if t[i]))
 
-        # Transpose `step` and `num_envs` dimensions and cast to torch tensors.
-        obs = torch.from_numpy(obs).transpose(1, 0)
-        actions = torch.from_numpy(actions).transpose(1, 0)
-        rewards = torch.from_numpy(rewards).transpose(1, 0)
-        done = torch.from_numpy(done).transpose(1, 0)
+            # If there is only one env, then stop the loop when done.
+            if num_envs == 1 and done[s].all():
+                obs, actions, rewards, next_obs, done = \
+                  obs[:s], actions[:s], rewards[:s], next_obs[:s], done[:s]
+                break
 
-        # Pass the experiences to the agent to update the policy.
-        agent.update(obs, actions, rewards, done)
+        # Pass the experiences to the agent to update the policy. We have to
+        # transpose `step` and `num_envs` dimensions and cast to torch tensors.
+        agent.update(
+            torch.from_numpy(obs.swapaxes(1, 0)),
+            torch.from_numpy(actions.T),
+            torch.from_numpy(rewards.T),
+            torch.from_numpy(next_obs.swapaxes(1, 0)),
+            torch.from_numpy(done.T),
+        )
 
         # Bookkeeping.
         assert len(episode_returns) == len(episode_lengths), "lengths must match"
@@ -91,9 +112,10 @@ def environment_loop(seed, agent, env, num_iters, steps, log_dir, log_every=1, d
             run_ret = r if run_ret is np.nan else 0.99 * run_ret + 0.01 * r
             run_len = l if run_len is np.nan else 0.99 * run_len + 0.01 * l
         with warnings.catch_warnings():
-            # We might finish the rollout without completing any episodes. Then
-            # taking the mean or std of an empty slice throws a runtime warning.
-            # As a result we would get a NaN, which is exactly what we want.
+            # We might finish the rollout without completing any episodes. In
+            # this case we want to store NaN in the history. Taking the mean or
+            # std of an empty slice throws a runtime warning and returns a NaN,
+            # which is exactly what we want.
             warnings.simplefilter("ignore", category=RuntimeWarning)
             avg_r, avg_l = np.array(episode_returns).mean(), np.array(episode_lengths).mean()
             std_r, std_l = np.array(episode_returns).std(), np.array(episode_lengths).std()
@@ -122,5 +144,39 @@ def environment_loop(seed, agent, env, num_iters, steps, log_dir, log_every=1, d
     torch.save(agent, os.path.join(log_dir, "agent.pt"))
     with open(os.path.join(log_dir, "train_history.pickle"), "wb") as f:
         pickle.dump(agent.train_history, f, protocol=pickle.HIGHEST_PROTOCOL)
+
+    # Generate plots from the training and save them in the log folder.
+    generate_plots(log_dir, agent.train_history)
+
+
+def generate_plots(log_dir, train_history):
+    num_iters = len(train_history)
+    plt.style.use("ggplot")
+    train_history[0]["Return"]["test_avg"] = train_history[0]["Return"]["avg"]
+    for k in train_history[0].keys():
+        fig, ax = plt.subplots()
+        if "avg" in train_history[0][k].keys():
+            xs = np.array([i for i in range(num_iters) if "avg" in train_history[i][k].keys()])
+            avg = np.array([train_history[i][k]["avg"] for i in xs])
+            ax.plot(xs, avg, label="Average")
+        if "std" in train_history[0][k].keys():
+            std = np.array([train_history[i][k]["std"]
+                for i in range(num_iters) if "std" in train_history[i][k].keys()])
+            ax.fill_between(xs, avg-0.5*std, avg+0.5*std, color="k", alpha=0.25)
+        if "run" in train_history[0][k].keys():
+            run = np.array([train_history[i][k]["run"] for i in range(num_iters)])
+            ax.plot(run, label="Running")
+        if "test_avg" in train_history[0][k].keys():
+            xs = np.array([i for i in range(num_iters) if "test_avg" in train_history[i][k].keys()])
+            test_avg = np.array([train_history[i][k]["test_avg"] for i in xs])
+            ax.plot(xs, test_avg, label="Test Average")
+        if "test_std" in train_history[0][k].keys():
+            test_std = np.array([train_history[i][k]["test_std"]
+                for i in range(num_iters) if "test_std" in train_history[i][k].keys()])
+            ax.fill_between(xs, test_avg-0.5*test_std, test_avg+0.5*test_std, color="k", alpha=0.25)
+        ax.legend(loc="upper left")
+        ax.set_xlabel("Number of training iterations")
+        ax.set_ylabel(k)
+        fig.savefig(os.path.join(log_dir, k.replace(" ", "_")+".png"))
 
 #
